0fe4957ea24bb826a3b0fc795b5ee134bd91ea94
BasitAyaz <basitahmed1997@gmail.com>
2024-06-08 17:25:42 +0500

    8-6-2024 (QUEEE Firebase, Project)

--- src/config/firebaseconfig.ts
+++ src/config/firebaseconfig.ts
@@ -6,18 +6,10 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBw2zQyHsG_LcHGQqTk63AaJF9gUk0MI0w",
-  authDomain: "queee-1a9e5.firebaseapp.com",
-  projectId: "queee-1a9e5",
-  storageBucket: "queee-1a9e5.appspot.com",
-  messagingSenderId: "119080454249",
-  appId: "1:119080454249:web:81a4b5ded8832c610f3af8",
-  measurementId: "G-JWG4FPBP4V"
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export default app
+export default app;
